c3ea9318c473524d58096ea4a34911340c2939dc
genuinelycool <genuinelycool@gmail.com>
2025-11-13 19:15:29 +0545

    15.1 -Design Frontend Case Studies Option Part 1 - fix missing container div

--- src/components/CaseStudy/CaseStudy.jsx
+++ src/components/CaseStudy/CaseStudy.jsx
@@ -6,21 +6,23 @@ const CaseStudy = () => {
   return (
     <div>
       <section className="case-study-block lg:pt-[100px] sm:pt-16 pt-10">
-        <div className="heading text-center">
-          <h3 className="heading3">Case Studies</h3>
-          <div className="right flex flex-col items-center gap-2 mt-3">
-            <div className="body3">
-              Experience the excitement and potential of the cryptocurrency
-              market with our expert trading services.
-            </div>
-            <Link
-              className="flex items-center gap-2 hover:text-blue 
+        <div className="container">
+          <div className="heading text-center">
+            <h3 className="heading3">Case Studies</h3>
+            <div className="right flex flex-col items-center gap-2 mt-3">
+              <div className="body3">
+                Experience the excitement and potential of the cryptocurrency
+                market with our expert trading services.
+              </div>
+              <Link
+                className="flex items-center gap-2 hover:text-blue 
                 duration-300"
-              href="/"
-            >
-              <div className="text-button">View All Case List</div>
-              <Icon.CaretDoubleRight weight="bold" className="text-xs mt-1" />
-            </Link>
+                href="/"
+              >
+                <div className="text-button">View All Case List</div>
+                <Icon.CaretDoubleRight weight="bold" className="text-xs mt-1" />
+              </Link>
+            </div>
           </div>
         </div>
       </section>
