8121b485d269fd1b2461cb36684eea75c4ea34d8
genuinelycool <genuinelycool@gmail.com>
2025-12-06 02:46:22 +0545

    47.1 -Call Backend API in Next js Frontend-

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domain: ["http://backend.test", "127.0.0.1", "localhost"],
+  },
+};
 
 export default nextConfig;
---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+const API_BASE_URL = "http://backend.test/api";
+const IMAGE_BASE_URL = "http://backend.test";
+
+export { API_BASE_URL, IMAGE_BASE_URL };
