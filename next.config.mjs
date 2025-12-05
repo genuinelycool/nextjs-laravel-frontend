de864f5621f335f12818e006037e5061988ebd99
genuinelycool <genuinelycool@gmail.com>
2025-12-06 03:59:38 +0545

    2 -Call Slider API to Frontend Page Part 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["http://backend.test", "127.0.0.1", "localhost"],
+    domains: ["backend.test", "127.0.0.1", "localhost"],
   },
 };
 
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,7 +7,8 @@ import * as Icon from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
 import Link from "next/link";
 import "swiper/css/bundle";
-import { API_BASE_URL } from "@/config/config";
+import { API_BASE_URL, IMAGE_BASE_URL } from "@/config/config";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Slider = () => {
   const [sliders, setSliders] = useState([]);
@@ -32,144 +33,97 @@ const Slider = () => {
 
   return (
     <>
-      <div className="slider-block">
-        <div className="prev-arrow items-center justify-center">
-          <Icon.CaretLeft className="text-white heading6" weight="bold" />
+      {loading ? (
+        <div className="flex justify-center items-center h-[500px]">
+          <ClipLoader color="#3498db" size={50} />
         </div>
+      ) : (
+        <div className="slider-block">
+          <div className="prev-arrow items-center justify-center">
+            <Icon.CaretLeft className="text-white heading6" weight="bold" />
+          </div>
 
-        <div className="slider-main">
-          <Swiper
-            spaceBetween={0}
-            slidesPerView={1}
-            navigation={{
-              prevEl: ".prev-arrow",
-              nextEl: ".next-arrow",
-            }}
-            loop={true}
-            pagination={{ clickable: true }}
-            speed={400}
-            modules={[Pagination, Autoplay, Navigation]}
-            className="h-full relative"
-            autoplay={{
-              delay: 4000,
-            }}
-          >
-            <SwiperSlide>
-              <div className="slider-item slider-first">
-                <div className="bg-img">
-                  <Image
-                    src={"/images/slider/one.jpg"}
-                    width={4000}
-                    height={3000}
-                    alt="slider1"
-                    priority={true}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-
-                <div className="container">
-                  <div className="text-content flex-column-between">
-                    <div className="heading2">
-                      <div className="relative overflow-hidden">
-                        <span className="block relative overflow-hidden">
-                          Simplify and Secure
-                        </span>
-                        <span className="block absolute top-0 left-0 w-full h-full">
-                          Simplify and Secure
-                        </span>
-                      </div>
-
-                      <div className="relative overflow-hidden">
-                        <span className="block relative overflow-hidden">
-                          Our Solution
-                        </span>
-                        <span className="block absolute top-0 left-0 w-full h-full">
-                          Our Solution
-                        </span>
-                      </div>
+          <div className="slider-main">
+            <Swiper
+              spaceBetween={0}
+              slidesPerView={1}
+              navigation={{
+                prevEl: ".prev-arrow",
+                nextEl: ".next-arrow",
+              }}
+              loop={true}
+              pagination={{ clickable: true }}
+              speed={400}
+              modules={[Pagination, Autoplay, Navigation]}
+              className="h-full relative"
+              autoplay={{
+                delay: 4000,
+              }}
+            >
+              {sliders.map((slider) => (
+                <SwiperSlide key={slider.id}>
+                  <div className="slider-item slider-first">
+                    <div className="bg-img">
+                      <Image
+                        src={`${IMAGE_BASE_URL}/${slider.image}`}
+                        width={4000}
+                        height={3000}
+                        alt="slider1"
+                        priority={true}
+                        className="w-full h-full object-cover"
+                      />
                     </div>
 
-                    <div className="body2 mt-3 text-secondary">
-                      Empower your business with our innovative solutions,
-                      Empower your business with our innovative solutions,
-                      <br />
-                      Empower your business with our innovative solutions,
-                    </div>
+                    <div className="container">
+                      <div className="text-content flex-column-between">
+                        <div className="heading2">
+                          <div className="relative overflow-hidden">
+                            <span className="block relative overflow-hidden">
+                              Simplify and Secure
+                            </span>
+                            <span className="block absolute top-0 left-0 w-full h-full">
+                              Simplify and Secure
+                            </span>
+                          </div>
 
-                    <div className="button-block md:mt-10 mt-6">
-                      <Link
-                        className="button-main bg-blue-700 text-white hover:bg-blue-500"
-                        href="/service"
-                      >
-                        Discovery Now
-                      </Link>
+                          <div className="relative overflow-hidden">
+                            <span className="block relative overflow-hidden">
+                              Our Solution
+                            </span>
+                            <span className="block absolute top-0 left-0 w-full h-full">
+                              Our Solution
+                            </span>
+                          </div>
+                        </div>
+
+                        <div className="body2 mt-3 text-secondary">
+                          Empower your business with our innovative solutions,
+                          Empower your business with our innovative solutions,
+                          <br />
+                          Empower your business with our innovative solutions,
+                        </div>
+
+                        <div className="button-block md:mt-10 mt-6">
+                          <Link
+                            className="button-main bg-blue-700 text-white hover:bg-blue-500"
+                            href="/service"
+                          >
+                            Discovery Now
+                          </Link>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            </SwiperSlide>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
 
-            <SwiperSlide>
-              <div className="slider-item slider-first">
-                <div className="bg-img">
-                  <Image
-                    src={"/images/slider/two.jpg"}
-                    width={4000}
-                    height={3000}
-                    alt="slider1"
-                    priority={true}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-
-                <div className="container">
-                  <div className="text-content flex-column-between">
-                    <div className="heading2">
-                      <div className="relative overflow-hidden">
-                        <span className="block relative overflow-hidden">
-                          Easy to Use
-                        </span>
-                        <span className="block absolute top-0 left-0 w-full h-full">
-                          Easy to Use
-                        </span>
-                      </div>
-
-                      <div className="relative overflow-hidden">
-                        <span className="block relative overflow-hidden">
-                          Our Solution
-                        </span>
-                        <span className="block absolute top-0 left-0 w-full h-full">
-                          Our Solution
-                        </span>
-                      </div>
-                    </div>
-
-                    <div className="body2 mt-3 text-secondary">
-                      Empower your business with our innovative solutions,
-                      Empower your business with our innovative solutions,
-                      <br />
-                      Empower your business with our innovative solutions,
-                    </div>
-
-                    <div className="button-block md:mt-10 mt-6">
-                      <Link
-                        className="button-main bg-blue-700 text-white hover:bg-blue-500"
-                        href="/service"
-                      >
-                        Discovery Now
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide>
-          </Swiper>
+          <div className="next-arrow items-center justify-center">
+            <Icon.CaretRight className="text-white heading6" weight="bold" />
+          </div>
         </div>
-
-        <div className="next-arrow items-center justify-center">
-          <Icon.CaretRight className="text-white heading6" weight="bold" />
-        </div>
-      </div>
+      )}
     </>
   );
 };
